36f82b223a861f631e9d0fb45c12cd152c819b6c
Raquel Dalete dos Santos Martins <raquel.martins@slserve.com.br>
2024-10-12 22:47:00 -0300

    segunda versão do portfolio

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+/*
+Objetivo 1 - quando o usuáro clicar no botão de mostrar mais, deve abrir os projetos que estão escondidos no html
+
+    Passo 1- pegar o botão mostrar mais no js pra poder verificr quando o usuário clicar em cima dele
+
+    Passo 2- identificar o clique no botão
+
+    Passo 3- adicionar a classe ativo nos projetos escondidos
+
+Objetivo 2- esconder o botão mostrar mais
+
+    Passo 1- pegar o botão e esconder ele
+*/
+
+console.log(document.querySelector('.btn-mostrar-projetos'));
